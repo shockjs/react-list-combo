b27ed8dba405bf32c1e172c226317a2c0ea28efa
Damian Dennis <damiandennis@gmail.com>
2016-01-01 19:23:19 +1000

    fix for relative path.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const ListView = require('dist/components/ListView');
-const GridView = require('dist/components/GridView');
-const GridColumn = require('dist/components/GridColumn');
-const Pagination = require('dist/components/Pagination');
+const ListView = require('./dist/components/ListView');
+const GridView = require('./dist/components/GridView');
+const GridColumn = require('./dist/components/GridColumn');
+const Pagination = require('./dist/components/Pagination');
 
 module.exports = {
   ListView: ListView,
