1b1d543b4a2c60c1301b1bcf1d1b2fc469f25d58
Damian Dennis <damiandennis@gmail.com>
2016-01-12 06:28:50 +1000

    added missing listrows.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const ListView = require('./dist/components/ListView');
 const GridView = require('./dist/components/GridView');
 const GridColumn = require('./dist/components/GridColumn');
 const Pagination = require('./dist/components/Pagination');
+const ListRows = require('./dist/components/ListRows');
 
 module.exports = {
+  ListRows: ListRows.default,
   ListView: ListView.default,
   GridView: GridView.default,
   GridColumn: GridColumn.default,
